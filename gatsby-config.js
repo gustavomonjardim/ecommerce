e50343dcbd583a6442373a82b356fc7fd9eb57a5
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-02-25 20:40:38 -0300

    chore: adiciona configuração do tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-postcss',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,6 +26,14 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
       },
     },
     'gatsby-plugin-netlify-cms',

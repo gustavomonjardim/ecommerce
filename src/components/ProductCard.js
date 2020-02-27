6926d45140fcc1f82fc0a5498ca37a5d81f66b90
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-02-27 11:15:58 -0300

    feat: adiciona listagem de produtos dinâmica

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,20 @@
 import { Link } from 'gatsby';
+import propTypes from 'prop-types';
 import React from 'react';
 
-import productImg from '../assets/images/product.jpg';
 import BagIcon from '../assets/svg/BagIcon';
-const ProductCard = () => {
+import { currencyMask } from '../services/maskService';
+
+const ProductCard = ({ link, name, price, image }) => {
   return (
-    <Link to="/" className="cursor-pointer">
+    <Link to={link} className="cursor-pointer">
       <div className="relative pb-4/3">
-        <img className="absolute w-full h-full object-cover" src={productImg} alt="" />
+        <img className="absolute w-full h-full object-cover" src={image} alt={name} />
       </div>
       <div className="flex flex-row justify-between items-start my-4">
         <div>
-          <h4 className="text-sm text-black font-thin">Nome do Produto</h4>
-          <p className="text-md text-gray-600 font-thin">R$ 24,00</p>
+          <h4 className="text-sm text-black font-thin">{name}</h4>
+          <p className="text-md text-gray-600 font-thin">{currencyMask(price)}</p>
         </div>
         <button type="button" className="cursor-pointer text-black h-5 w-5 focus:outline-none">
           <BagIcon />
@@ -20,6 +22,13 @@ const ProductCard = () => {
       </div>
     </Link>
   );
+};
+
+ProductCard.propTypes = {
+  link: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  price: propTypes.number.isRequired,
+  image: propTypes.object.isRequired,
 };
 
 export default ProductCard;

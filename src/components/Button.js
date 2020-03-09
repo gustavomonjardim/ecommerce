2e8deee56d30dea9ef156872e7f9fec6faa8e8cc
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-03-09 19:04:56 -0300

    feat: adiciona hover no button

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,7 @@ const Button = ({ text, onClick, loading }) => {
       {!loading && (
         <button
           onClick={onClick}
-          className="w-full bg-black text-white px-4 py-2 focus:outline-none font-semibold h-12"
+          className="w-full bg-black text-white px-4 py-2 focus:outline-none font-semibold h-12 hover:bg-gray-900"
         >
           {text}
         </button>

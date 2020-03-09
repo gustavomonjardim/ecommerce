268dbd8a7d1e43f55b6b59d28969a785cfe27edd
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-03-08 22:25:55 -0300

    feat: cria Loader e adiciona loading no Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,32 @@
 import propTypes from 'prop-types';
 import React from 'react';
 
-const Button = ({ text, onClick }) => {
+import Loader from '../components/Loader';
+
+const Button = ({ text, onClick, loading }) => {
   return (
-    <button
-      onClick={onClick}
-      className="w-full bg-black text-white px-4 py-2 focus:outline-none font-semibold h-12"
-    >
-      {text}
-    </button>
+    <>
+      {!loading && (
+        <button
+          onClick={onClick}
+          className="w-full bg-black text-white px-4 py-2 focus:outline-none font-semibold h-12"
+        >
+          {text}
+        </button>
+      )}
+      {loading && <Loader />}
+    </>
   );
 };
 
 Button.propTypes = {
   text: propTypes.string.isRequired,
   onClick: propTypes.func.isRequired,
+  loading: propTypes.bool,
+};
+
+Button.defaultProps = {
+  loading: false,
 };
 
 export default Button;

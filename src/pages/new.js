6ddcff6a7634a3dd4756d81752002ea9a9844430
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-03-15 18:40:11 -0300

    refactor: atualiza query das paginas new e on-sale

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,19 +1,30 @@
 import { graphql } from 'gatsby';
 import propTypes from 'prop-types';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import ProductCard from '../components/ProductCard';
 import Layout from '../layouts/Layout';
 
 const Shop = ({ data }) => {
+  const products = useMemo(() => {
+    return data.allMarkdownRemark.nodes.map(product => ({
+      id: product.id,
+      image: product.frontmatter.image,
+      price: product.frontmatter.price,
+      description: product.frontmatter.description,
+      name: product.frontmatter.name,
+      seller: product.frontmatter.seller,
+      slug: product.fields.slug,
+    }));
+  }, [data]);
   return (
-    <Layout title="On Sale">
+    <Layout title="Shop">
       <div className="w-full">
         <h1 className="text-black font-thin text-5xl md:text-6xl mb-12">
-          Plants.<span className="font-sans text-green-600">New</span>
+          Plants.<span className="text-green-600">All</span>
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12">
-          {data.allProductsJson.nodes.map(product => (
+          {products.map(product => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
@@ -24,18 +35,26 @@ const Shop = ({ data }) => {
 
 export const query = graphql`
   query {
-    allProductsJson(filter: { new: { eq: true } }) {
+    allMarkdownRemark(filter: { frontmatter: { new: { eq: true } } }) {
       nodes {
         id
-        name
-        price
-        image
         fields {
           slug
         }
-        seller {
-          id
+        frontmatter {
           name
+          price
+          image {
+            childImageSharp {
+              fluid(maxWidth: 272, maxHeight: 363, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          seller {
+            id
+            name
+          }
         }
       }
     }
@@ -44,7 +63,7 @@ export const query = graphql`
 
 Shop.propTypes = {
   data: propTypes.shape({
-    allProductsJson: propTypes.shape({
+    allMarkdownRemark: propTypes.shape({
       nodes: propTypes.array,
     }).isRequired,
   }).isRequired,
